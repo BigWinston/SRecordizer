861417f787d5478b92a9a5799e059162185140d1
okapui <92432738+okapui@users.noreply.github.com>
2021-10-14 10:23:42 +0900

    Add high-dpi support.

--- Program.cs
+++ Program.cs
@@ -7,12 +7,16 @@ namespace SRecordizer
 {
     static class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            SetProcessDPIAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SRecordizer());
